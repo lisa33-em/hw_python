8c853e3623ba6c0b896fd2e32f091fe1f25c1d99
bodyaginaelizaveta <embodyagina@gmail.com>
2026-04-16 23:48:54 +0300

    hw-67

--- part5_decorators/hw67.py
+++ part5_decorators/hw67.py
@@ -1,4 +1,6 @@
 import json
+from datetime import datetime, timezone
+from functools import wraps
 from typing import Any, ParamSpec, Protocol, TypeVar
 from urllib.request import urlopen
 
@@ -20,20 +22,74 @@ class CallableWithMeta(Protocol[P, R_co]):
 
 
 class BreakerError(Exception):
-    pass
+    def __init__(self, func_name: str, block_time: datetime):
+        super().__init__(TOO_MUCH)
+        self.func_name = func_name
+        self.block_time = block_time
 
 
 class CircuitBreaker:
     def __init__(
         self,
-        critical_count: int,
-        time_to_recover: int,
-        triggers_on: type[Exception],
-    ): ...
+        critical_count: int = 5,
+        time_to_recover: int = 30,
+        triggers_on: type[Exception] = Exception,
+    ):
+        exceptions = []
+
+        if (critical_count <= 0 or not isinstance(critical_count, int)):
+            exceptions.append(ValueError(INVALID_CRITICAL_COUNT))
+
+        if (time_to_recover <= 0 or not isinstance(time_to_recover, int)):
+            exceptions.append(ValueError(INVALID_RECOVERY_TIME))
+
+        if len(exceptions) > 0:
+            raise ExceptionGroup(VALIDATIONS_FAILED, exceptions)
+
+        self.critical_count = critical_count
+        self.time_to_recover = time_to_recover
+        self.triggers_on = triggers_on
+        self.count = 0
+        self.when_blocked: datetime | None = None
 
     def __call__(self, func: CallableWithMeta[P, R_co]) -> CallableWithMeta[P, R_co]:
-        raise NotImplementedError
+        self._func_name = f"{func.__module__}.{func.__name__}"
 
+        @wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R_co:
+            if self.when_blocked is not None:
+                if self.has_recovered():
+                    self.count = 0
+                    self.when_blocked = None
+                else:
+                    raise BreakerError(self._func_name, self.when_blocked)
+
+            try:
+                result = func(*args, **kwargs)
+
+            except self.triggers_on as error:
+                self.handle_error(error)
+                raise
+
+            else:
+                self.count = 0
+                return result
+
+        return wrapper
+
+    def has_recovered(self) -> bool:
+        current_time = datetime.now(timezone.UTC)
+        when_blocked = self.when_blocked
+
+        diff = (current_time - when_blocked).total_seconds()
+
+        return diff >= self.time_to_recover
+
+    def handle_error(self, error: Exception) -> None:
+        self.count += 1
+        if self.count >= self.critical_count:
+            self.when_blocked = datetime.now(timezone.UTC)
+            raise BreakerError(self._func_name, self.when_blocked) from error
 
 circuit_breaker = CircuitBreaker(5, 30, Exception)
 
